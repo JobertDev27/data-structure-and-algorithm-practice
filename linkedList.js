50232315a1d04ac1d776ddc6e17d5d77320101eb
JobertDev27 <jobertdev27@gmail.com>
2025-06-17 17:06:31 +0800

    fix: constructor wrong asigning

--- linkedList.js
+++ linkedList.js
@@ -1,25 +1,57 @@
 class List {
-  constructor(root, tail) {
-    root = null;
-    tail = null;
+  constructor() {
+    this.root = null;
+    this.end = null;
   }
 
   append(value) {
+    const item = new Item();
+    item.setValue(value);
+
     if (!this.root) {
-      const item = new Item();
-      item.value = value;
-      this.head = item.value;
+      this.root = item;
+      this.end = item;
     }
+    this.end.setNext(item);
+    this.end = item;
   }
 
   head() {
-    return this.root;
+    return this.root.getValue();
+  }
+
+  tail() {
+    return this.end.getValue();
   }
 }
 
 class Item {
-  constructor(value, next) {
-    value = null;
-    next = null;
+  constructor() {
+    this.value = null;
+    this.next = null;
+  }
+
+  setValue(value) {
+    this.value = value;
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  setNext(value) {
+    this.next = value;
+  }
+
+  getNext() {
+    return this.next;
   }
 }
+
+const list = new List();
+list.append("hello");
+console.log(list.head());
+console.log(list.tail());
+list.append("hi");
+console.log(list.head());
+console.log(list.tail());
